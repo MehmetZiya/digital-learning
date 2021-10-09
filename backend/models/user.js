34f7089cf4f7a501efec3019de7ba39bdf9adf02
Mehmet Ziya <mehmetziya82@gmail.com>
2021-10-09 22:05:10 +0200

    added user and course model

---
+++ backend/models/user.js
@@ -0,0 +1,46 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  surname: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: false
+  },
+  img: {
+    type: String,
+    required: true
+  },
+  wishList: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Course"
+    },
+  ],
+  purchased: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Course"
+    },
+  ],
+  owner: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Course"
+    },
+  ]
+});
+
+module.exports = mongoose.model("User", userSchema);
