663f4f8a2c394d190c9872beef1551ffeec84e4d
Mehmet Ziya <mehmetziya82@gmail.com>
2021-10-10 10:44:06 +0200

    added users query

--- backend/graphql/resolvers/course.js
+++ backend/graphql/resolvers/course.js
@@ -1,4 +1,4 @@
-const User = require("../../models/user");
+//const User = require("../../models/user");
 const Course = require("../../models/course");
 
 const { transformCourse } = require('./merge');
--- backend/graphql/resolvers/user.js
+++ backend/graphql/resolvers/user.js
@@ -3,6 +3,19 @@ const bcrypt = require("bcryptjs");
 const User = require("../../models/user");
 
 module.exports = {
+  users: async () => {
+    try {
+      const users = await User.find();
+      return users.map((user) => {
+        return {
+          ...user._doc,
+          _id: user.id
+        };
+      });
+    } catch (err) {
+      throw err;
+    }
+  },
   createUser: async (args) => {
     try {
       const existingUser = await User.findOne({ email: args.userInput.email });
