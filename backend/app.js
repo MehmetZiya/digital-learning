5efaec94b6541eb0d4d9dd5961b7eaa3d078eb4e
Mehmet Ziya <mehmetziya82@gmail.com>
2021-10-10 20:13:46 +0200

    changed category model

--- backend/app.js
+++ backend/app.js
@@ -1,33 +1,32 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const graphqlHTTP = require('express-graphql').graphqlHTTP;
+const graphqlHTTP = require("express-graphql");
 const mongoose = require("mongoose");
 
 //graphQL schema and functions
-const graphQlSchema = require('./graphql/schema/index');
-const graphQlResolvers = require('./graphql/resolvers/index');
-
+const graphQlSchema = require("./graphql/schema/index");
+const graphQlResolvers = require("./graphql/resolvers/index");
 
 const app = express();
 app.use(bodyParser.json());
 
 // graphQL setup
 app.use(
-  '/graphql',
+  "/graphql",
   graphqlHTTP({
     schema: graphQlSchema,
     rootValue: graphQlResolvers,
-    graphiql: true
+    graphiql: true,
   })
 );
 
 mongoose
   .connect(
     `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.1t7vp.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`
-  ) 
+  )
   .then(() => {
     app.listen(5000);
-    console.log("Mongo DB connected!")
+    console.log("Mongo DB connected!");
   })
   .catch((err) => {
     console.log(err);
