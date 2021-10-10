1ec4b03bfdd84df70e2a7fd9311c855f4e2e82f7
celiltat2361 <celilbtc@gmail.com>
2021-10-10 19:00:33 +0200

    addded category schema and resolvers

---
+++ backend/graphql/resolvers/category.js
@@ -0,0 +1,20 @@
+const Category = require("../../models/category");
+
+module.exports = {
+  categories: async () => {
+    try {
+      const categories = await Category.find();
+      return categories.map((category) => {
+        return {
+          ...categories._doc,
+          _id: category.id,
+          name: category.name,
+          subCategory: category.subCategory
+        };
+      });
+    } catch (err) {
+      throw err;
+    }
+  },
+
+};
--- backend/graphql/resolvers/index.js
+++ backend/graphql/resolvers/index.js
@@ -1,10 +1,12 @@
 const userResolver = require('./user');
 const courseResolver = require('./course');
+const categoryResolver = require('./category');
 
 
 const rootResolver = {
   ...userResolver,
   ...courseResolver,
+  ...categoryResolver,
 };
 
 module.exports = rootResolver;
