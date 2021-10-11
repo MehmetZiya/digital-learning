ac465e41bc25d39f8637e40baa30791df7b4e5b6
Mehmet Ziya <mehmetziya82@gmail.com>
2021-10-11 16:09:33 +0200

    added routing ,navbar, user register and login front side

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,28 @@
+import { Redirect, Route, Switch } from "react-router-dom";
 import Register from './pages/Register' ;
-
+import HomePage from './pages/HomePage' ;
+import Login from './pages/Login' ;
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
-      <h1>Initial project files</h1>
-      <Register />
+      <Navbar />
+      <Switch>
+        <Route exact path="/">
+            <Redirect to ="/home" />
+          </Route>
+          <Route exact path="/home">
+            <HomePage />
+          </Route>
+          <Route exact path="/register">
+            <Register />
+          </Route>
+          <Route exact path="/login">
+            <Login />
+          </Route>
+          
+        </Switch>
     </div>
   );
 }
